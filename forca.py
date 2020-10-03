6b5165cddba53a07198f1d5ba431261f17fc4d26
Bruno Souza <brunojose1234.bj@gmail.com>
2020-10-03 12:41:09 -0300

    adicionando o primeiro arquivo do jogo forca ainda em andamento

---
+++ forca.py
@@ -0,0 +1,16 @@
+def jogar():
+    print("*********************************")
+    print("Bem-vindo ao jogo de forca!")
+    print("*********************************")
+
+    palavra_secreta = "show"
+    enforcou = False
+    errou = False
+
+    while(not enforcou and not errou):
+        print("Jogando...")
+
+    print("Fim de jogo!")
+
+if(__name__ == "__main__"):
+    jogar()
